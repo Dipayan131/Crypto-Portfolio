49f15fb041c301aed3e9fce254d5278717cef14c
Dipayan Roy <roy.dipayan131@gmail.com>
2024-08-20 00:51:50 +0530

    Background Image Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   const {walletConnectStatus} = useContext(AppContext);
 
   return (
-    <div className="relative min-h-screen bg-cover bg-center overflow-hidden" style={{ backgroundImage: 'url(/Image/cyberpunk-bitcoin-illustration_23-2151611192.jpg)' }}>
+    <div className="relative min-h-screen bg-cover bg-center overflow-hidden" style={{ backgroundImage: 'url(./Image/cyberpunk-bitcoin-illustration_23-2151611192.jpg)' }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <Router>
       <div className="relative z-10">
