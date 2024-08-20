9fcd0e35118834aa21d62befa3a5c1109b3a52d3
Dipayan Roy <roy.dipayan131@gmail.com>
2024-08-20 15:39:08 +0530

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,7 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AppContext } from '../../contexts/AppContext'
 import "./Navbar.css"
 
 export default function Navbar() {
-  const {walletConnectStatus,
-    setWalletConnectStatus,
-    defaultAccount,
-    setDefaultAccount,
-    userBalance,
-    setUserBalance} = useContext(AppContext)
-
-    const handleDisconnect = () => {
-      setWalletConnectStatus(false);
-      setDefaultAccount(null);
-      setUserBalance(null);
-    }
 
   return (
     <div className='w-full h-24 flex justify-between items-center border-b-4 border-white'>
@@ -23,11 +9,6 @@ export default function Navbar() {
       <span className='text-orange-600'>Crypto</span>
       <span className='text-black'>Folio</span>
 </div>
-
-      {walletConnectStatus && <Link to='/' onClick={() => handleDisconnect()} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded shadow-lg transition duration-300 ease-in-out mr-12">
-  Disconnect Wallet
-</Link>}
-
     </div>
   )
 }
